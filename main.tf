85e1260a1ec79a4edb4cd91c2abd53b148a163da
FernandoAraujoMarques <61606557+FernandoAraujoMarques@users.noreply.github.com>
2021-11-03 21:39:33 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,3 +29,25 @@ resource "aws_iam_role" "lambda_exec_role" {
 }
 EOF
 }
+
+
+resource "aws_iam_role" "ec2_lambda_exec_role" {
+  name        = "ec2_lambda_exec"
+  path        = "/"
+  description = "Allows Lambda Function to call AWS services on your behalf."
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
